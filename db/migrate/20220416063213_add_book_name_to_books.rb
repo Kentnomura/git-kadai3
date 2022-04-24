289e99e7588c93fd8faf809d2d6805e6b58d37ca
EC2 Default User <ec2-user@ip-172-31-11-11.ec2.internal>
2022-04-24 08:53:11 +0000

    finish-2

--- db/migrate/20220416063213_add_book_name_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddBookNameToBooks < ActiveRecord::Migration[6.1]
-  def change
-    add_column :books, :book_name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -41,9 +41,9 @@ ActiveRecord::Schema.define(version: 2022_04_23_053230) do
   end
 
   create_table "books", force: :cascade do |t|
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
     t.string "title"
     t.text "body"
   end
